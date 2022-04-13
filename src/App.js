d034fb5f300724843f520c10450886e9ae4710b8
David Morales <david.morales@soytul.com>
2022-04-12 21:35:41 -0500

    add config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
+import React, { useState, useEffect } from 'react';
+import { fabric } from 'fabric';
+const App = () => {
+  const [canvas, setCanvas] = useState('');
 
-function App() {
+  useEffect(() => {
+    setCanvas(initCanvas());
+  }, []);
+  const initCanvas = () => (
+    new fabric.Canvas('canvas', {
+      backgroundColor: 'pink'
+    })
+  )
   return (
-    <div className='h1'>
-      HOLO
-      <Button as={Col} variant="success">Button #3</Button>
+    <div>
+      <h1>Candy Creator</h1>
+      <div class="container bg-primary">
+        <div class="row ">
+          <div class="col">
+            <canvas id="canvas" height={'100%'} width={'100%'} />
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
